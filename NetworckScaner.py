92f5fb662f467256ccec5351b3ddc9d851521ed0
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-04-25 03:49:44 -0400

    algoritem part1 complite

--- NetworckScaner.py
+++ NetworckScaner.py
@@ -7,7 +7,10 @@ def scan(ip):
     ARPRequest = scapy.ARP(pdst = ip)
     Broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     ARPRequestBroadcast = Broadcast/ARPRequest
- 
+    answered, unanswered = scapy.srp(ARPRequestBroadcast, timeout=5)
+    print(answered.summary())
+
+
 
 #scan(ip/rang)
 scan("10.0.2.4/24")
