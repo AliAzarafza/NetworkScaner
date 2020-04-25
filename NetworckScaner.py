d85fa4fb053704f593a40e6eee89bd4606e8f3a4
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-04-25 04:41:18 -0400

    visual

--- NetworckScaner.py
+++ NetworckScaner.py
@@ -3,16 +3,26 @@
 #################
 import scapy.all as scapy
 
+
+#ARP recust and its resalts
 def scan(ip):
     ARPRequest = scapy.ARP(pdst = ip)
     Broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     ARPRequestBroadcast = Broadcast/ARPRequest
     answeredList = scapy.srp(ARPRequestBroadcast, timeout=5, verbose=False)[0]
-    print('___________________________________________\n ip\t\t\tMAC adress\n-----------------------------------')
+    clientList = []
     for element in answeredList:
-        print(element[1].psrc'\t\t'element[1].hwsrc)
+        clientDict = {"ip":element[1].psrc, "MAC":element[1].hwsrc}
+        clientList.append(clientDict)
+    return clientList
 
+#show resalts
+def printer(resaltsLIST):
+    print('___________________________________________\n ip\t\t\tMAC adress\n-----------------------------------')
+    for client in resaltsLIST:
+        print(client["ip"] + '\t\t' + client["MAC"])
 
 
 #scan(ip/rang)
-scan("10.0.2.4/24")
+scanResalt = scan("10.0.2.4/24")
+printer(scanResalt)
