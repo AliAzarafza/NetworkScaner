0e4695a3d40f6bd36c7bbb40855ce86e2f5db388
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-04-25 04:19:18 -0400

    program fontional

--- NetworckScaner.py
+++ NetworckScaner.py
@@ -7,11 +7,10 @@ def scan(ip):
     ARPRequest = scapy.ARP(pdst = ip)
     Broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     ARPRequestBroadcast = Broadcast/ARPRequest
-    answeredList = scapy.srp(ARPRequestBroadcast, timeout=5)[0]
+    answeredList = scapy.srp(ARPRequestBroadcast, timeout=5, verbose=False)[0]
+    print('___________________________________________\n ip\t\t\tMAC adress\n-----------------------------------')
     for element in answeredList:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("----------------------------------------------------------------------------")
+        print(element[1].psrc'\t\t'element[1].hwsrc)
 
 
 
